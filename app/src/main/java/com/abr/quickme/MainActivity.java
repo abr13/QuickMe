600d4f55d1ee96104f4e453fc55c4d02d0fc6674
ABR <abdurrahman131197@gmail.com>
2019-07-10 11:30:08 +0530

    added : request,chats,friends tabs using ViewPager

--- app/src/main/java/com/abr/quickme/MainActivity.java
+++ app/src/main/java/com/abr/quickme/MainActivity.java
@@ -7,7 +7,9 @@ import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -16,10 +18,20 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private FirebaseAuth mAuth;
 
+    private ViewPager mViewPager;
+    private MainPagerAdapter mPagerAdapter;
+
+    private TabLayout mTabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewPager = findViewById(R.id.main_tabPager);
+        mPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+        mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
---
+++ app/src/main/java/com/abr/quickme/MainPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.abr.quickme;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+class MainPagerAdapter extends FragmentPagerAdapter {
+
+    public MainPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                RequestFragment requestFragment = new RequestFragment();
+                return requestFragment;
+            case 1:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+            case 2:
+                FriendsFragment friendsFragment = new FriendsFragment();
+                return friendsFragment;
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "REQUEST";
+            case 1:
+                return "CHATS";
+            case 2:
+                return "FRIENDS";
+            default:
+                return null;
+        }
+    }
+}
